ffc39ba2826c98ecee87a9a30354326fa21c7671
littletaffer <melvincwmah@gmail.com>
2023-03-25 14:31:08 +0800

    Assignment
    1. Add new pet services and a pet controller.
    2. Specify another default profile.

---
+++ src/main/java/student/springframework/sfsdi/controllers/PetController.java
@@ -0,0 +1,17 @@
+package student.springframework.sfsdi.controllers;
+
+import org.springframework.stereotype.Controller;
+import student.springframework.sfsdi.services.PetService;
+
+@Controller
+public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
