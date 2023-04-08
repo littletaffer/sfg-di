3cabb850c36fae2f871c4f916f9a9ef70d751af4
littletaffer <melvincwmah@gmail.com>
2023-04-08 14:36:31 +0800

    DI in Java Configuration (English Greeting Service)

--- src/main/java/student/springframework/sfsdi/config/GreetingServiceConfig.java
+++ src/main/java/student/springframework/sfsdi/config/GreetingServiceConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import student.springframework.sfsdi.repositories.EnglishGreetingRepository;
+import student.springframework.sfsdi.repositories.EnglishGreetingRepositoryImpl;
 import student.springframework.sfsdi.services.*;
 
 @Configuration
@@ -13,10 +15,16 @@ public class GreetingServiceConfig {
     I18nSpanishGreetingService ii8NSpanishService() {
         return new I18nSpanishGreetingService();
     }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
     @Primary
     @Bean
--- src/main/java/student/springframework/sfsdi/services/I18nEnglishGreetingService.java
+++ src/main/java/student/springframework/sfsdi/services/I18nEnglishGreetingService.java
@@ -1,6 +1,14 @@
 package student.springframework.sfsdi.services;
 
+import student.springframework.sfsdi.repositories.EnglishGreetingRepository;
+
 public class I18nEnglishGreetingService implements GreetingService {
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
+    private final EnglishGreetingRepository englishGreetingRepository;
     @Override
     public String sayGreeting() {
         return "Hello World ----- EN";
